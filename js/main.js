5dc560c27bf75ba6cae9c10c6ecc146ee68eab35
Alan Diaz <82740575+NinjaDiaz007@users.noreply.github.com>
2024-11-21 17:36:51 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ let finality = document.getElementById("finality");
 let card = document.getElementById("card");
 let reboot = document.getElementById("reboot");
 
+let enviar = document.getElementById("enviar");
+let card2 = document.getElementById("card-2")
+let cont = 1;
+
+let Lista_Respuesta = new Array();
+
 let imgH = document.getElementById("img-h");
 let btn_hamburgesa = document.getElementById("lbl-hamburgesa");
 
@@ -101,6 +107,12 @@ btn_hamburgesa.addEventListener('click', ()=>{
     }
 });
 
+enviar.addEventListener('click', ()=>{
+    guardarArchivoDeTexto(Lista_Respuesta, "informacion.txt");
+    Cerrar_Modal();
+    card2.style.left = "50%";
+});
+
 // Funciones
 function Init(){
     PreguntasPantalla();
@@ -119,4 +131,14 @@ function PreguntasPantalla(){
 
 function Cerrar_Modal(){
     card.style.left = "-50%";
+}
+
+// Botones que son "si"
+function Precion_SI(argument) {
+	Lista_Respuesta.push(argument+". Si");
+}
+
+// Botones que son "no"
+function Precion_NO(argument) {
+	Lista_Respuesta.push(argument+". No");
 }
